649b9bdecf24e7ead0bffc634e9240ea4b1acb62
Phạm Quốc Tú <110210663+tupq48@users.noreply.github.com>
2024-10-23 21:15:09 +0700

    Update 12_determine-color-of-a-chessboard-square.py

--- 12_determine-color-of-a-chessboard-square.py
+++ 12_determine-color-of-a-chessboard-square.py
@@ -1,1 +1,5 @@
-
+class Solution:
+    def squareIsWhite(self, coordinates: str) -> bool:
+        letter = ord(coordinates[0]) - ord('a')  
+        number = int(coordinates[1])  
+        return (letter + number) % 2 == 0 
