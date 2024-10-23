684d009e4bdcc1d118090fecce8f7c782273f454
Phạm Quốc Tú <110210663+tupq48@users.noreply.github.com>
2024-10-23 21:09:58 +0700

    Create 11_count-equal-and-divisible-pairs-in-an-array.py

---
+++ 11_count-equal-and-divisible-pairs-in-an-array.py
@@ -0,0 +1,9 @@
+class Solution:
+    def countPairs(self, nums: List[int], k: int) -> int:
+        res = 0
+        for i in range(0, len(nums) - 1):
+            for j in range(i+1, len(nums)):
+                if (nums[i] == nums[j]) and ((i*j)%k==0):
+                    res += 1
+
+        return res
